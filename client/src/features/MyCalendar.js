38018059a6f70aa602c2b5f31f7df7e80795931f
Jiali Zhang <zhangjiali86@gmail.com>
2023-01-02 21:36:26 +0100

    delete console.log in Authenticaion & show name

--- client/src/features/MyCalendar.js
+++ client/src/features/MyCalendar.js
@@ -1,20 +1,29 @@
-import {Calendar, momentLocalizer} from "react-big-calendar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "../../node_modules/react-big-calendar/lib/css/react-big-calendar.css";
+import { useSelector } from "react-redux";
+import { getUser } from "./authentication/authenticationSlice";
 
 const localizer = momentLocalizer(moment)
 
 export const MyCalendar = (props) => {
+  const user = useSelector(getUser);
 
   return (
     <div>
-        <Calendar
+      {user.givenName && (
+        <div>
+          {`${user.givenName} ${user.familyName}' calendar`}
+        </div>
+      )}
+
+      <Calendar
         localizer={localizer}
         // events={""}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
-        />
+      />
     </div>
   )
 }
--- client/src/features/authentication/Authentication.js
+++ client/src/features/authentication/Authentication.js
@@ -1,20 +1,18 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
-import { useDispatch, useSelector } from "react-redux";
-import { authentication, getUser } from "./authenticationSlice";
+import { useDispatch} from "react-redux";
+import { authentication} from "./authenticationSlice";
 
 export const Authentication = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const user = useSelector(getUser);
+
   return (
     <div>
       <GoogleLogin
         onSuccess={credentialResponse => {
-          console.log(credentialResponse.credential);
-          var decoded = jwt_decode(credentialResponse.credential);
-          console.log(decoded);
+          const decoded = jwt_decode(credentialResponse.credential);
           dispatch(authentication({
             email: decoded.email,
             familyName: decoded.family_name,
