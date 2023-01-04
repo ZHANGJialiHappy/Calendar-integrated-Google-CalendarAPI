c979e6adb51ff547fb07d1728d854e9bf1e26878
Jiali Zhang <zhangjiali86@gmail.com>
2023-01-04 15:13:32 +0100

    createAsyncThunk

--- client/src/features/authentication/authenticationSlice.js
+++ client/src/features/authentication/authenticationSlice.js
@@ -1,10 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import Axios from "axios";
 
  const initialState = {
     email: "",
     familyName: "",
     givenName: "",
  } 
+
+ export const getEvents = createAsyncThunk(
+    'posts/getPosts',
+    async (thunkAPI) => {
+        const res = await Axios.get(`https://www.googleapis.com/calendar/v3/calendars/${getUser.email}/events`)
+        .then((data)=> data.json())
+        return res
+    }
+ )
+
 export const authenticationSlice = createSlice({
   name: 'authentication',
   initialState,
@@ -14,7 +25,20 @@ export const authenticationSlice = createSlice({
         state.familyName = action.payload.familyName;
         state.givenName = action.payload.givenName;
     }
-
+  },
+  extraReducers:{
+    [getEvents.pending]:(state)=>{
+        state.loading = true;
+    },
+    [getEvents.fulfilled]:(state, {payload})=>{
+        state.loading=false;
+        state.entities=payload;
+        state.isSuccess=true;
+    },
+    [getEvents.rejected]:(state)=>{
+        state.loading=false;
+        state.isSuccess=false;
+    },
   },
 })
 
